77425b74d6c691d66c6f3feb3acebfbaf3e17c10
Jay Islam <contact@jahirultusar.com>
2026-04-22 08:03:23 +0100

    Added main.tf file with backend and provider info

---
+++ main.tf
@@ -0,0 +1,27 @@
+# terraform backend info 
+# - usually goes to a seperate terraform.tf file
+# - keeping i here in main.tf as per trello instruction
+
+terraform {
+  backend "s3" {
+    bucket  = "jay-metapod-tf-state"
+    key     = "dev/terraform.tfstate"
+    region = "eu-west-2"
+    encrypt = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.2"
+}
+
+# defined rquired_providers such as AWS
+
+provider "aws" {
+  region = "eu-west-2"
+}
